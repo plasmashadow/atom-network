0725d0748c29290776aad59e81dd67157b78c526
plasmashadow <plasmashadowx@gmail.com>
2016-09-27 13:17:25 +0530

    Initial Commit

---
+++ spec/bandwidth-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import BandwidthView from '../lib/bandwidth-view';
+
+describe('BandwidthView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
